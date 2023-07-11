dbbdfd91b07102a8d5326a154fdd15a26084fcfe
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-07-11 11:45:25 +0900

    first commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,24 @@
+class QuestionsController < ApplicationController
+
+  def index
+   @questions = Question.all
+  end
+
+  def new
+   @question = Question.new
+  end
+
+  def create
+   Question.create(question_params)
+   redirect_to action: :index
+  end
+
+  def show
+   @question = Question.find(params[:id])
+  end
+
+  private 
+  def question_params
+   params.require(:question).permit(:title, :content, :name)
+  end
+end
